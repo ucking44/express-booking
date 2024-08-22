650a175de6d34e9f47ec0dfcc68df6ad80a62dc6
ucking44 <ucking4niga@yahoo.com>
2024-08-22 13:46:52 +0100

    More db

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
+    host: '',
+    user: '',
     password: '',
-    database: 'laravel_eloquent'
+    database: ''
 });
 
 db.connect((err) => {
